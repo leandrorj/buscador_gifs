a50b562195b3590f5eef88aa9c730dce3572938a
leandroIFarias <leandro.inacio.farias@gmail.com>
2019-01-06 17:07:06 -0300

    desenvolvimento da Grid de Gifs

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -68,10 +69,8 @@ class _HomePageState extends State<HomePage> {
                         ),
                       );
                     default:
-                      if (snapshot.hasError)
-                        return Container();
-                      else
-                        _createGifTable(context, snapshot);
+                      if (snapshot.hasError) return Container();
+                      else return _createGifTable(context, snapshot);
                   }
                 }),
           )
@@ -81,6 +80,23 @@ class _HomePageState extends State<HomePage> {
   }
 }
 
-Widget _createGifTable (BuildContext context,AsyncSnapshot snapshot){
+Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot) {
+  return GridView.builder(
+      padding: EdgeInsets.all(10.0),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 10.0,
+        mainAxisSpacing: 10.0,
+      ),
+      itemCount: snapshot.data["data"].length,
+      itemBuilder: (context, index) {
+        return GestureDetector(
+          child: Image.network(
+            snapshot.data["data"][index]["images"]["fixed_height"]["url"],
+            height: 300.0,
+            fit: BoxFit.cover,
+          ),
 
+        );
+      });
 }
